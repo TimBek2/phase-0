afaadcaee6ff81982f4bc86c61956e88ba107d1f
TimBek2 <tim.beck46@gmail.com>
2015-12-30 22:25:40 -0500

    Update hours spent

--- errors.rb
+++ errors.rb
@@ -1,7 +1,7 @@
 # Analyze the Errors
 
 # I worked on this challenge by myself.
-# I spent [#] hours on this challenge.  9:11 PM
+# I spent [1.5] hours on this challenge.  9:11 PM
 
 # --- error -------------------------------------------------------
 
