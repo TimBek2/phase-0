83e650ba9a3853ef7aafa345c869bd0b3810b2f3
TimBek2 <tim.beck46@gmail.com>
2016-01-17 02:49:11 -0500

    Initial commit, solve rough code for guessing game

---
+++ week-6/guessing-game/my_solution.rb
@@ -0,0 +1,84 @@
+# Build a simple guessing game
+
+
+# I worked on this challenge by myself
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input: Call the 
+# Output:
+# Steps:
+
+
+# Initial Solution
+
+class GuessingGame
+  def initialize(answer)
+  	@answer = answer
+  end
+
+# Make sure you define the other required methods, too
+  def guess(guess)
+  	@guess = guess
+  	if guess > @answer
+  		return :high
+  	elsif @guess == @answer
+  		return :correct
+  	elsif @guess < @answer
+  		return :low
+  	end
+  end
+
+  def solved?
+  	if @guess == @answer
+  		return true
+  	else
+  		return false
+  	end
+  end
+
+
+end
+
+
+
+
+
+
+# Refactored Solution
+
+# class GuessingGame
+#   def initialize(answer)
+#   	@answer = answer
+#   	attr_reader :answer
+#   end
+
+# # Make sure you define the other required methods, too
+#   def guess(guess)
+#   	guess = @guess
+#   	if @guess > :answer
+#   		return :low
+#   	elsif @guess == :answer
+#   		return :correct
+#   	elsif @guess < :answer
+#   		return :high
+#   	end
+#   end
+
+#   def solved?
+#   	if @guess == :answer
+#   		return true
+#   	else
+#   		return false
+#   	end
+#   end
+
+
+# end
+
+
+
+
+
+# Reflection
