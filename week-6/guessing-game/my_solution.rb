0cc07cc16a44ccece6e7d5640ff271240391cdd4
TimBek2 <tim.beck46@gmail.com>
2016-01-17 03:43:37 -0500

    Final commit, add reflection content

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -14,13 +14,13 @@
 # Initial Solution
 
 # class GuessingGame
-#   def initialize(answer)
-#   	@answer = answer
-#   end
+  # def initialize(answer)
+  # 	@answer = answer
+  # end
 
 # # Make sure you define the other required methods, too
 #   def guess(guess)
-#   	@guess = guess
+#   	@guess == guess
 #   	if guess > @answer
 #   		return :high
 #   	elsif @guess == @answer
@@ -49,19 +49,19 @@
 # Refactored Solution
 
 class GuessingGame
-  # def initialize(answer)
-  	attr_reader :answer
-  # end
+  def initialize(answer)
+  	@answer = answer
+  end
 
 # Make sure you define the other required methods, too
   def guess(guess)
   	@guess = guess
-  	if @guess > @answer
+  	if guess > @answer
   		return :high
-  	elsif @guess == @answer
-  		return :correct
-  	elsif @guess < @answer
+  	elsif guess < @answer
   		return :low
+  	else
+  		return :correct
   	end
   end
 
@@ -81,3 +81,27 @@ end
 
 
 # Reflection
+=begin
+
+How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+Instance variables are able to hold the values for different traits of the object and carry them from one method to the next, while instance methods that you create will define the actions that the object can take. In this case, there were variables saving the correct answer and the guess, and two methods to compare the guess to the answer and to determine if the problem has been solved. 
+
+When should you use instance variables? What do they do for you?
+Instance variables are used any time you need to carry information from one method to the next. They allow you to store information in an object that won't disappear as soon as that one method is finished. Often our work until this point has only required information to be operated upon and then the answer is passed - if you ever need to save that answer and access it later, instance variables are likely your choice. 
+
+Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+Flow control is using logic gates in order to have an input cycle through your code and return the correct output. By this point in the curriculum, I'm not having any trouble with flow control anymore. Sometimes there will be a roadblock when the logic gates aren't ordered correctly, which usually means that I accidentally put my catch-all clause at the front of the statement and it's skipping over everything else, but that's quickly caught at this point. 
+
+Why do you think this code requires you to return symbols? What are the benefits of using symbols?	
+Symbols are useful when you are using a lot of "boilerplate" text in your code. Symbols, unlike other variables, only occur in one place, and every time you call it, you are calling that specific object. It's useful in cases with lots of repetitive text because it will prevent the computer from using up unnecessary memeory by storing 50 (or 5000) different variables named "high" when they all indicate the same thing. This problem could have that situation, because the answer could be a million, and there could be a million guesses up till then. By using symbols, it's as if the computer could simply compare the two numbers and then point at high, low, or correct, rather than having to write out "high", "low" or "correct" and save that result for each guess. 
+
+
+	
+=end
+
+
+
+
+
+
+
