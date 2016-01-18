75c1e2d811776d53859c733491603b098594bc83
TimBek2 <tim.beck46@gmail.com>
2016-01-17 22:02:33 -0500

    Finish Release 0

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -1,6 +1,6 @@
 #Attr Methods
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge by myself
 
 # I spent [#] hours on this challenge.
 
@@ -22,3 +22,22 @@ end
 
 
 # Reflection
+=begin
+
+# Release 0
+
+What are these methods doing?
+All of the what_is_ methods simply return the values associated with each variable, and the change_my_ methods allow you to assign a new value to each variable. 
+
+How are they modifying or returning the value of instance variables?
+Because it is an instance variable denoted with @, it exists within the entire class object. What_is methods just return the value, while change_my methods take and argument and set the variable equal to that. 
+
+# Release 1
+
+
+
+=end
+
+
+
+
