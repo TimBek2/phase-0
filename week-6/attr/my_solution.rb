4d703c89a05c6461820aa3da5e63ab2b9134234d
TimBek2 <tim.beck46@gmail.com>
2016-01-18 00:33:06 -0500

    Final commit, finished reflection

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -41,7 +41,7 @@ puts test.hello
 # Reflection
 =begin
 
-# Release 0
+# Release 1
 
 What are these methods doing?
 All of the what_is_ methods simply return the values associated with each variable, and the change_my_ methods allow you to assign a new value to each variable. 
@@ -49,7 +49,7 @@ All of the what_is_ methods simply return the values associated with each variab
 How are they modifying or returning the value of instance variables?
 Because it is an instance variable denoted with @, it exists within the entire class object. What_is methods just return the value, while change_my methods take and argument and set the variable equal to that. 
 
-# Release 1
+# Release 2
 
 What changed between the last release and this release?
 The code attr_reader :age was added, and the code from line 24-26 was removed, which was the method to return the value of @age.
@@ -60,7 +60,7 @@ The method what_is_age was replaced with attr_reader :age
 Is this code simpler than the last?
 Yes, it only uses one line rather than 3, additionally taking away the opportunity to forget to end a method and break the code :)
 
-# Release 2
+# Release 3
 
 What changed between the last release and this release?
 The code attr_writer :age was added, and the method change_my_age was removed.
@@ -71,16 +71,23 @@ The code attr_writer :age was added, and the method change_my_age was removed.
 Is this code simpler than the last?
 Yes, as before you took out another method with a single, consistent line of code. 
 
-# Release 3
+# Final Reflection
 
+What is a reader method?
+A reader method is a method that provides access to a class attribute. It creates an instance variable that other methods can access the value of. 
 
+What is a writer method?
+The writer method is like the reader method, but it allows you to change the value of the variable instead of access it. 
 
+What do the attr methods do for you?
+The attr methods allow you to quickly access or change a class attribute succinctly. The reader method uses the code attr_reader :variable, while the writer method uses the code attr_writer :variable. 
 
+Should you always use an accessor to cover your bases? Why or why not?
+Using an accessor can be helpful because it can help to infer the intent of a variable or method, but I would hesitate to say that they're useful from covering bases standpoint. 
 
+What is confusing to you about these methods?
+At first I tried to use the writer method for the final challenge in my solution, but I couldn't figure out how to get an object to initialize with my name as it's @name value, and then run the code properly. I'll need to keep working on how these operate. 
 
 
 =end
-
-
-
 
