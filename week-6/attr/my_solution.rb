79928e127db5364d0a43288a3dab6a1c5acb18a6
TimBek2 <tim.beck46@gmail.com>
2016-01-17 22:28:35 -0500

    Third commit, add reflection content for release 2

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -38,10 +38,23 @@ What changed between the last release and this release?
 The code attr_reader :age was added, and the code from line 24-26 was removed, which was the method to return the value of @age.
 
 What was replaced?
-THe method what_is_age was replaced with attr_reader :age
+The method what_is_age was replaced with attr_reader :age
 
 Is this code simpler than the last?
 Yes, it only uses one line rather than 3, additionally taking away the opportunity to forget to end a method and break the code :)
+
+# Release 2
+
+What changed between the last release and this release?
+The code attr_writer :age was added, and the method change_my_age was removed.
+
+What was replaced?
+The code attr_writer :age was added, and the method change_my_age was removed.
+
+Is this code simpler than the last?
+Yes, as before you took out another method with a single, consistent line of code. 
+
+
 
 =end
 
