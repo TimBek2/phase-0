c25f91a9c73f722409f12bf7f58bc62ef4317370
TimBek2 <tim.beck46@gmail.com>
2016-01-24 17:50:15 -0500

    Second to last commit, add reflection content

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -4,26 +4,40 @@
 
 // Program Structure
 // Write your own variable and do something to it.
-var name = "Tim"
-console.log(name + " rocks")
+var name = "Tim";
+console.log(name + " rocks");
 
-
+// Food program
+// var favfood = prompt("What\'s your favorite food?", "");
+// alert("Hey! That's my favorite too!");
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
 // Loop a triangle
-var triangle = ""
-var counter = 1
+var triangle = "";
+var counter = 1;
 
 while (counter <= 7) {
-	counter += 1
-	console.log(triangle += "#")
-}
+	counter += 1;
+	console.log(triangle += "#");
+};
 
 // Functions
 
 // Complete the `minimum` exercise.
-
-
+function min(num1, num2){
+	numArray = [num1, num2].sort();
+	minimum = numArray.splice(1, 1);
+	console.log(minimum);
+};
+min(7,444)
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+var me = {
+	name:"Tim",
+	age:25,
+	favFoods:["Steak", "Eggs", "Pizza"],
+	quirk:"I can juggle"
+};
+
+console.log(me.name)
